c2442f464abd0766e54765c45b7d1820cdcda181
nicosigler <59884109+nicosigler@users.noreply.github.com>
2020-02-12 22:06:56 -0500

    This is the completed assignment

--- Scripts/Ship.gd
+++ Scripts/Ship.gd
@@ -4,5 +4,12 @@ extends KinematicBody2D
 func _ready():
 	pass
 
-func _physics_process(delta):
-	pass
+func _physics_process(_delta):
+	if Input.is_action_pressed("left"):
+		position.x = position.x - 10
+	if Input.is_action_pressed("right"):
+		position.x = position.x + 10
+	if Input.is_action_pressed("down"):
+		position.y = position.y + 10
+	if Input.is_action_pressed("up"):
+		position.y = position.y - 10 
